8f73351b9e3298ac8012d1db8f33de58b68092e2
TrebledJ <39648915+TrebledJ@users.noreply.github.com>
2023-05-06 16:20:47 +0800

    implement basic midi note loading

--- assets/js/util.js
+++ assets/js/util.js
@@ -1,46 +1,61 @@
-
-
-
-const roll = $('webaudio-pianoroll')[0];
-
+const roll = $("webaudio-pianoroll")[0];
 
 class MidiUtils {
-    static loadFromFile(file) {
-        console.log('got file:', file.name);
-        if (!['wav', 'mid'].includes(file.name.split('.').pop())) {
-            throw new Error(`Unrecognised file type in '${file}'.`);
-        }
+  static loadFromFile(file) {
+    console.log("got file:", file.name);
+    if (!["wav", "mid"].includes(file.name.split(".").pop())) {
+      throw new Error(`Unrecognised file type in '${file}'.`);
+    }
 
-        var reader = new FileReader();
-        reader.onload = function(e) {
-            try {
-                if (file.name.endsWith('.wav')) {
-                    // TODO: check
-                    MidiUtils.loadFromWAV(e.target.result);
-                } else if (file.name.endsWith('.mid')) {
-                    MidiUtils.loadFromMIDI(e.target.result);
-                }
-            } catch (err) {
-                console.error(err);
-                alert("Failed to load the file. :(");
-            }
-        };
-        reader.readAsArrayBuffer(file);
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      try {
+        if (file.name.endsWith(".wav")) {
+          // TODO: check
+          MidiUtils.loadFromWAV(e.target.result);
+        } else if (file.name.endsWith(".mid")) {
+          MidiUtils.loadFromMIDI(e.target.result);
+        }
+      } catch (err) {
+        console.error(err);
+        alert("Failed to load the file. :(");
+      }
+    };
+    reader.readAsArrayBuffer(file);
+  }
+
+  static loadFromMIDI(data) {
+    // Parse into object: https://github.com/Tonejs/Midi/tree/master#format.
+    const midi = new Midi(data);
+    // console.log(JSON.stringify(midi));
+
+    let seq = roll.sequence;
+    seq.splice(0, seq.length); // Delete contents.
+
+    const addNote = (note, vel, tick, dur) => {
+      seq.push({ n: note, t: tick, g: dur, v: vel, f: 0 });
     };
 
-    static loadFromMIDI(data) {
-        const midi = new Midi(data);
-        console.log(JSON.stringify(midi));
-    }
+    const bpm = midi.header.tempos[0].bpm;
 
-    static loadFromWAV(wav) {
-        console.log("loadFromWAV is unimplemented.");
-        // TODO.
-    }
-};
+    midi.tracks.forEach((track) => {
+      const { name, channel, notes } = track;
+
+      notes.forEach(({ midi, velocity, ticks, durationTicks }) => {
+        addNote(midi, velocity, Math.round(ticks / bpm), Math.round(durationTicks / bpm));
+      });
+      const endTicks = track.endOfTrackTicks;
+    });
+
+    roll.redraw();
+  }
+
+  static loadFromWAV(wav) {
+    console.log("loadFromWAV is unimplemented.");
+    // TODO.
+  }
+}
 
 // $(function () {
-
-    
 
 // });
